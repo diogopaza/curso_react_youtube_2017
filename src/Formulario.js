15e4115c0052aeacb178c9322e9dc293917a2f0e
Diogo Paza <diogopazacvel@gmail.com>
2017-12-22 11:15:19 -0200

    segundo commit

---
+++ src/Formulario.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+export default class extends Component{
+
+    render(){
+
+        return(
+            
+            <form>
+                <input type="text" placeholder="" />
+
+            </form>
+
+        );
+    };
+
+};
--- src/index.js
+++ src/index.js
@@ -1,42 +1,25 @@
 import ReactDOM from 'react-dom';
-import React, {Component} from 'react';
+import react, {Component} from 'react';
+import Formulario from './Formulario';
+
+
 
 
 class App extends Component{
-    
-    constructor(){
 
-        super();
-        this.state = {
-            contador : 0
-        };
-    };
-
-    somarContador(){
-
-        this.setState({
-           contador : this.state.contador +  1 
-        })
-    }
-    
-    render(){
-
+   render(){
         return(
-            <div>
-                  <p>A soma é {this.state.contador}</p> 
-                    <button onClick={() => this.somarContador()}>Somar</button>     
+            <Formulario />
 
-            </div>
-         
         );
-    };
 
+   };
 };
 
-class LoginForm extends Component{
 
 
-};
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
